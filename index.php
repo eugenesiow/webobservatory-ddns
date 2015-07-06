9ba0cdc0163400578606d7b25ac21292b17e0630
Eugene Siow <kyo116@gmail.com>
2015-07-06 22:16:08 +0100

    Add redirect to WO

--- index.php
+++ index.php
@@ -2,7 +2,36 @@
 require 'vendor/autoload.php';
 
 Flight::route('/', function(){
-    echo "nothing here";
+    $authEndpoint = 'https://webobservatory.soton.ac.uk/oauth/authorise';
+    $params = http_build_query(array(
+        'response_type'=>'token',
+        'client_id'=>'559ae81d0dc1819807f1244f',
+        'redirect_uri'=>'http://webobservatory.me/home'
+    ));
+    Flight::redirect($authEndpoint.'?'.$params);
+
+});
+
+Flight::route('/home', function(){
+    var_dump(Flight::request()->getBody());
+    echo '<br><br>home'.Flight::request()->data->token_type;
+});
+
+Flight::route('/user', function(){
+    //open connection
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL, 'http://webobservatory.soton.ac.uk/api/userInfo');
+    curl_setopt($ch,CURLOPT_HTTPHEADER,array (
+        "Authorization:Bearer "
+    ));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //execute get
+    $result = curl_exec($ch);
+
+    //close connection
+    curl_close($ch);
+
+    echo $result;
 });
 
 Flight::start();
